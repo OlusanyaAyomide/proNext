dd04928eb541f88cfb39d1263e6cafb7fb27b38c
Olusanya Ayomide <ayomideflex72@gmail.com>
2024-07-09 10:00:56 +0100

    add-logo

--- src/components/util-component/Logo.tsx
+++ src/components/util-component/Logo.tsx
@@ -3,7 +3,7 @@ import { cn } from '../lib/utils'
 
 export default function Logo({className,style}:{className?:string,style?:string}) {
   return (
-    <div className={cn('w-[260px] h-[65px] scale-150 pt-1',style)}>
+    <div className={cn('w-[110px] h-[45px] pt-1',style)}>
         <img className={cn("object-contain h-full w-full",className)} src='/logo3.png' alt=''></img>
     </div>
   )
